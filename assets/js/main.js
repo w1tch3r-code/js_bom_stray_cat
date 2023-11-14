d7400e581157112c2f81c86a40e71c057e58b625
w1tch3r-code <rainerbrain@gmail.com>
2023-11-14 22:57:47 +0100

    JS Aufgabe BOM – Stray Cat fertig gestellt

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+"use strict";
+
+// ===================================================
+//      	 JS-Vertiefung – BOM Stray Cat
+// ===================================================
+
+console.log("%c JS-Vertiefung – BOM Stray Cat", "color: tomato");
+
+let pos = 0;
+let rt = 2;
+let motor;
+const cat = document.querySelector("#cat");
+
+const animation = () => {
+	cat.style.left = `${pos}px`;
+	pos += rt;
+
+	const windowWidth = window.innerWidth;
+
+	if (pos >= windowWidth - cat.clientWidth) {
+		cat.style.transform = "scaleX(-1)";
+		rt = -2;
+	}
+
+	if (pos <= 0) {
+		cat.style.transform = "scaleX(1)";
+		rt = 2;
+	}
+};
+
+const catWalk = () => {
+	pause();
+	motor = setInterval(animation, 20);
+};
+
+const pause = () => {
+	clearInterval(motor);
+};
+
+const turn = () => {
+	const currentScaleX = cat.style.transform;
+
+	if (currentScaleX === "" || currentScaleX === "scaleX(1)") {
+		cat.style.transform = "scaleX(-1)";
+		rt = -Math.abs(rt); // Stelle sicher, dass die Katze vorwärts läuft
+	} else {
+		cat.style.transform = "scaleX(1)";
+		rt = Math.abs(rt); // Stelle sicher, dass die Katze vorwärts läuft
+	}
+};
+
+const catSpeed = () => {
+	pause();
+	motor = setInterval(animation);
+};
+
+// Füge ein Event-Listener für die Veränderung der Fensterbreite hinzu
+window.addEventListener("resize", () => {
+	// Setze die Position auf den Anfang, wenn das Fenster neu skaliert wird
+	pos = 0;
+});
